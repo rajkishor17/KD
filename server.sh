36ff0c136169e6dae52539d7147d95b42d1b5bc9
rajkishor17 <rajkishor17@gmail.com>
2026-04-08 17:08:01 +0530

    Delete server.sh

--- server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  NODE_ENV=production NODE_OPTIONS="--max-old-space-size=2048" npx next start -p 3000 </dev/null >/tmp/next-prod.log 2>&1
-  sleep 2
-done
